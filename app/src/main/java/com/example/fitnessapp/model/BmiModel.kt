a96d345e3e7c82e58c4509d2119980e69db7cb6e
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-03-02 23:03:51 +0545

    Bmimodel added

---
+++ app/src/main/java/com/example/fitnessapp/model/BmiModel.kt
@@ -0,0 +1,11 @@
+package com.example.fitnessapp.model
+
+data class BmiModel(
+    val bmiId: String = "",
+    val userId: String = "",
+    val weight: String = "",
+    val height: String = "",
+    val bmi: String = "",
+    val status: String = "",
+    val date: String = ""
+)
