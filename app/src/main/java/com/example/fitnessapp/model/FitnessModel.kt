d64e04ab1d605530b27700450a7131967df7e048
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-03-02 23:07:56 +0545

    added FitnessModel

---
+++ app/src/main/java/com/example/fitnessapp/model/FitnessModel.kt
@@ -0,0 +1,14 @@
+package com.example.fitnessapp.model
+
+data class FitnessModel(
+    val fitnessId: String = "",
+    val userId: String = "",
+    val weight: String = "",
+    val height: String = "",
+    val bmi: String = "",
+    val protein: String = "",
+    val carbs: String = "",
+    val fat: String = "",
+    val totalCalories: String = "",
+    val date: String = ""
+)
