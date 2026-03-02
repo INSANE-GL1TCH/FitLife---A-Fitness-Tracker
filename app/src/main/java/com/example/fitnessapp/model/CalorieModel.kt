1db9950ef4d13a3ceb5411db1a9eacd8ed3ebbbd
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-03-02 23:06:48 +0545

    added caloriemodel

---
+++ app/src/main/java/com/example/fitnessapp/model/CalorieModel.kt
@@ -0,0 +1,12 @@
+package com.example.fitnessapp.model
+
+data class CalorieModel(
+    val calorieId: String = "",
+    val userId: String = "",
+    val calorieGoal: String = "",
+    val protein: String = "",
+    val carbs: String = "",
+    val fat: String = "",
+    val totalCalories: String = "",
+    val date: String = ""
+)
