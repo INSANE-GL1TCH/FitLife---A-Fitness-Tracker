4bfdc1069128932140fe59be473ae6f7b059dbc9
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-03-03 13:23:07 +0545

    made the Ui better

--- app/src/main/java/com/example/fitnessapp/model/CalorieModel.kt
+++ app/src/main/java/com/example/fitnessapp/model/CalorieModel.kt
@@ -4,6 +4,7 @@ data class CalorieModel(
     val calorieId: String = "",
     val userId: String = "",
     val mealName: String = "",
+    val mealCategory: String = "Snack", // e.g., Breakfast, Lunch, Dinner, Snack
     val calorieGoal: String = "2000",
     val protein: String = "",
     val carbs: String = "",
@@ -14,6 +15,7 @@ data class CalorieModel(
     fun toMap(): Map<String, Any?> {
         return mapOf(
             "mealName" to mealName,
+            "mealCategory" to mealCategory,
             "calorieGoal" to calorieGoal,
             "protein" to protein,
             "carbs" to carbs,
