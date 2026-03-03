bb1ec1aec9cdc8d168f067c3b381517a926a36e2
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-03-03 13:16:13 +0545

    fix error

--- app/src/main/java/com/example/fitnessapp/model/CalorieModel.kt
+++ app/src/main/java/com/example/fitnessapp/model/CalorieModel.kt
@@ -3,10 +3,23 @@ package com.example.fitnessapp.model
 data class CalorieModel(
     val calorieId: String = "",
     val userId: String = "",
-    val calorieGoal: String = "",
+    val mealName: String = "",
+    val calorieGoal: String = "2000",
     val protein: String = "",
     val carbs: String = "",
     val fat: String = "",
     val totalCalories: String = "",
-    val date: String = ""
-)
+    val date: String = "" // Format: dd MMM yyyy
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "mealName" to mealName,
+            "calorieGoal" to calorieGoal,
+            "protein" to protein,
+            "carbs" to carbs,
+            "fat" to fat,
+            "totalCalories" to totalCalories,
+            "date" to date
+        )
+    }
+}
